b7684175850ae39bd4c24e962c3516cf8bbc221b
HamzaGbada <gbadahamza18@gmail.com>
2021-03-19 21:26:21 +0100

    [Fix] Otsu thresholding

--- Service/PreProcessService.py
+++ Service/PreProcessService.py
@@ -1,20 +1,35 @@
-import cv2
-from Mapper.Normalization import Normalization
-import logging as log
 import numpy as np
-from math import sqrt
+
 
 class PreProcess:
 
-    def OtsuThresholding(pixel_data, max):
-        pixel_number = pixel_data.shape[0] * pixel_data.shape[1]
+    def OtsuThresholding(self, max):
+        """This function calculate the Otsu thresholding of image in Dicom file.
+            For more information about the Otsu method see this link: https://en.wikipedia.org/wiki/Otsu%27s_method
+
+        Args:
+            self: pixel_array in Dicom file.
+            max: Maximum value that can be assigned to a pixel.
+
+        Returns:
+            The return value is the thresholded image.
+
+        """
+
+        # Number of pixels in an image
+        pixel_number = self.shape[0] * self.shape[1]
+
         mean_weight = 1.0 / pixel_number
 
-        his, bins = np.histogram(pixel_data, np.arange(0, max+1))
+        # Histogram Calculation
+        his, bins = np.histogram(self, np.arange(0, max + 1))
+
+        # Initialization
         final_thresh = -1
         final_value = -1
-        intensity_arr = np.arange(max)
-        for t in bins[1:-1]:  # This goes from 1 to max-1 uint8 range (Pretty sure wont be those values)
+
+        for t in bins[1:-1]:
+
             pc1 = np.sum(his[:t])
             pc2 = np.sum(his[t:])
 
@@ -24,22 +39,24 @@ class PreProcess:
             P1 = pc1 * mean_weight
             P2 = pc2 * mean_weight
 
-            mu1 = pc1 / float(w1)
-            mu2 = np.sum(intensity_arr[t:] * his[t:]) / float(w1)
-            # print mub, muf
-            sigma1 = np.sum(((intensity_arr[:t] * his[:t])-mu1) ** 2 ) / float(w1)
-            sigma2 = np.sum(((intensity_arr[t:] * his[t:]) - mu2) ** 2) / float(w2)
-            # value = P1 * P2 * (mu1 - mu2) ** 2
-            value = P1*sigma1 + P2*sigma2
-            value = sqrt(value)
+            # Mean class calculation
+            mu1 = np.sum(his[:t]) / float(w1)
+            mu2 = np.sum(his[t:]) / float(w2)
+
+            # Interclass variance
+            value = P1 * P2 * (mu1 - mu2) ** 2
+
+            # Determination of the threshold
             if value > final_value:
                 final_thresh = t
                 final_value = value
-        final_img = pixel_data.copy()
-        # print('azertyuiop')
-        # print(final_thresh)
-        final_img[pixel_data > final_thresh] = max
-        final_img[pixel_data < final_thresh] = 0
+
+        final_img = self.copy()
+
+        # Binarization
+        final_img[self > final_thresh] = max
+        final_img[self < final_thresh] = 0
+
         return final_img
 
     def GammaBlur(self):
