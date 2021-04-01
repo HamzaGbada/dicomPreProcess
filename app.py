9548b379602ed57b5a85e1422788ebeed198cffa
HamzaGbada <gbadahamza18@gmail.com>
2021-04-01 17:54:39 +0100

    CORS set up

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask_restful import Api
 from Controller.otsuthreshold import OtsuThreshold
 from Controller.gammaCorrection import GammaCorrection
 from Controller.contrastAdjust import ContrastAdjust
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 api.add_resource(GammaCorrection,'/gammaCorrection/<float:gamma>')
 api.add_resource(OtsuThreshold,'/otsuThreshold/<int:max>')
