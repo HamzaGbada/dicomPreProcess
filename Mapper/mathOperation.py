617ca417375798763755b4e4f12951765de795e5
HamzaGbada <gbadahamza18@gmail.com>
2021-04-30 16:20:12 +0100

    Gabor methods

--- Mapper/mathOperation.py
+++ Mapper/mathOperation.py
@@ -16,9 +16,36 @@ class PixelArrayOperation:
         matrix = x * self
         return matrix.astype(np.uint16)
 
-    def convolution(self):
-        pass
+    def convolution(oldimage, kernel):
+        kernel_h = kernel.shape[0]
+        kernel_w = kernel.shape[1]
 
+        if (len(oldimage.shape) == 3):
+            image_pad = np.pad(oldimage,
+                               pad_width=((kernel_h // 2, kernel_h // 2), (kernel_w // 2, kernel_w // 2), (0, 0)),
+                               mode='constant', constant_values=0).astype(np.float32)
+        elif (len(oldimage.shape) == 2):
+            image_pad = np.pad(oldimage, pad_width=(
+                (kernel_h // 2, kernel_h // 2), (kernel_w // 2, kernel_w // 2)), mode='constant',
+                               constant_values=0).astype(np.float32)
+        h = kernel_h // 2
+        w = kernel_w // 2
+
+        image_conv = np.zeros(image_pad.shape)
+
+        for i in range(h, image_pad.shape[0] - h):
+            for j in range(w, image_pad.shape[1] - w):
+                x = image_pad[i - h:i - h + kernel_h, j - w:j - w + kernel_w]
+                x = x.flatten() * kernel.flatten()
+                image_conv[i][j] = x.sum()
+        h_end = -h
+        w_end = -w
+
+        if (h == 0):
+            return image_conv[h:, w:w_end]
+        if (w == 0):
+            return image_conv[h:h_end, w:]
+        return image_conv[h:h_end, w:w_end]
 
 class InformationTheory:
 
@@ -47,3 +74,5 @@ class InformationTheory:
         mi = InformationTheory.entropy(pixel_data) + InformationTheory.entropy(dest_data) - InformationTheory.joint_entropy(pixel_data, dest_data)
 
         return mi
+
+
--- Service/GRAIL.py
+++ Service/GRAIL.py
@@ -1,10 +1,40 @@
 #  أليلتنا بذي حسم انيري *** إذا انت انقضيت فلا تحوري
 #  فإن يكن بالذنائب طال ليلي *** فقد ابكي من الليل القصيري
+import numpy as np
+from Mapper.mathOperation import PixelArrayOperation
+
 
 class GRAIL:
-    def gabor_filter(self):
-        pass
+
+    @classmethod
+    def gabor_kernel(self, kernel_size, sigma, gamma, lamda, psi, angle):
+        gabor_kernel = np.zeros((kernel_size, kernel_size), np.float32)
+        m = kernel_size // 2
+        n = kernel_size // 2
+        # degree -> radian
+        theta = angle / 180. * np.pi
+        for i in range(-m, m + 1):
+            for j in range(-n, n + 1):
+                # get kernel x
+                x = np.cos(theta) * i + np.sin(theta) * j
+                # get kernel y
+                y = -np.sin(theta) * i + np.cos(theta) * j
+
+                gabor_kernel[i + m, j + n] = np.exp(-(x ** 2 + gamma ** 2 * y ** 2) / (2 * sigma ** 2)) * np.cos(
+                    2 * np.pi * x / lamda + psi)
+
+        return gabor_kernel
+
+    def gabor_response(pixel_data, kernel_size, sigma, gamma, lamda, psi, theta):
+
+        kernel = GRAIL.gabor_kernel(kernel_size, sigma, gamma, lamda, psi, theta)
+        im_filtered = np.zeros(pixel_data.shape, dtype=np.float32)
+        im_filtered[:, :] = PixelArrayOperation.convolution(pixel_data[:, :], kernel)
+
+        return im_filtered
+
     def quality_measurement(self):
         pass
+
     def get_best_A_B(self):
         pass
