270204c8219e6e7fac0e7f9b3816d942b6a7a68b
HamzaGbada <hamza.gbada@eniso.u-sousse.tn>
2021-08-26 12:39:10 +0100

    requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from Mapper.mathOperation import PixelArrayOperation
 from Service.Model import Data, Methode
 import logging
+from args import argument
 
+# Logger Setup
 logging.basicConfig(filename="grail.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
@@ -11,24 +13,23 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
-    var = input("give algorithm name:\n")
+
     image = Data.load_dicom_image("data/20587054.dcm")
     array = image.array
     logger.debug("Pixel Data \n {}".format(array))
     logger.debug("Pixel Data shape \n {}".format(image.shape))
-    if var == 'grail':
+    if argument.algorithm == 'grail':
         output = Data.grail_main(array)
-    if var == 'fedbs' :
-        method = input('give methode name:\n')
-        if method == 'dog':
+    if argument.algorithm == 'fedbs' :
+        if argument.method == 'dog':
             fedbs_array = Data.fedbs_main(Methode.DOG, array)
             array = PixelArrayOperation.getROI(array, 1250, 2000)
             output = PixelArrayOperation.getROI(fedbs_array, 1250, 2000)
-        if method == 'log':
+        if argument.method == 'log':
             fedbs_array = Data.fedbs_main(Methode.LOG, array)
             array = PixelArrayOperation.getROI(array, 1250, 2000)
             output = PixelArrayOperation.getROI(fedbs_array, 1250, 2000)
-        if method == 'bbp':
+        if argument.method == 'bbp':
             array = PixelArrayOperation.getROI(array, 1250, 2000)
             output = Data.fedbs_main(Methode.FFT, array)
 
